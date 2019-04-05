3011ea32232ebce1d2d3e4a96312c7f34944a788
yevhen orlov <yevhenorlov@gmail.com>
2019-04-05 23:52:09 +0300

    setup boilerplate

---
+++ src/ClockClass.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import './Clock.css'
+
+const Clock = class {}
+
+export default Clock
---
+++ src/ClockFunction.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import './Clock.css'
+
+const Clock = () => {}
+
+export default Clock
